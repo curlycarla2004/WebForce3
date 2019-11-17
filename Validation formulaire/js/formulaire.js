a037db599f5af0028fb99aef1ae41bbbf54525cc
curlycarla2004 <57147268+curlycarla2004@users.noreply.github.com>
2019-11-17 17:22:08 +0100

    Update formulaire.js

--- Validation formulaire/js/formulaire.js
+++ Validation formulaire/js/formulaire.js
@@ -2,30 +2,30 @@ window.onload = function(){
 
 let pass1 = document.getElementById('pass1');
 let pass2 = document.getElementById('pass2');
-let goodColor = "green";
-let badColor = "red";
+
 
 function validate password()
 {
     if(pass1.value.length > 5)
     {
-        pass1.style.backgroundColor = "goodColor";
+        pass1.style.backgroundColor = "green";
     }
     else
     {
-        pass1.style.backgroundColor = "badColor";
+        pass1.style.backgroundColor = "red";
         return;
     }
   
     if(pass1.value == pass2.value)
     {
-        pass2.style.backgroundColor = "goodColor";
+        pass2.style.backgroundColor = "green";
     }
 	else
     {
-        pass2.style.backgroundColor = "badColor";
+        pass2.style.backgroundColor = "red";
 
     }
 }  
+
 
 }//Window onload
