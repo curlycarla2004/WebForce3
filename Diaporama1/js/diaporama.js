b919c1a748b9751324a12b56982b9c494c7550a9
curlycarla2004 <curlycarla2004@hotmail.com>
2019-11-19 00:26:25 +0100

    change home

--- Diaporama1/js/diaporama.js
+++ Diaporama1/js/diaporama.js
@@ -1,32 +1,23 @@
-//Les tableaux
-//Déclarer un tableau
-let monJoliTableau = [123, "Bonjour", true];
-console.log(monJoliTableau);
+//let diaporama = ["images2", "img1", "metsky"];
+//for(let ligne = 0; ligne < diaporama.length; ligne++){ 
+    //console.log(diaporama[ligne])
+//}
 
-//Afficher "Bonjour" dans la console
-console.log(monJoliTableau[1]);
+var i = 0;
+var images = [];
+var timle= 3000;
 
-//Ajouter une valeur "red" à la fin de mon tableau
-monJoliTableau.push("red"); //.push will push at the end the valeur
-console.log(monJoliTableau);
+images[0] = "img1.jpg";
+images[1] = "image2.jpg";
+images[2] = "metsky.jpg";
 
-//Ajouter la valeur "#f00" au début  du tableau
-monJoliTableau.unshift("#f00"); // .unshift will put the valeur at the beginning
-console.log(monJoliTableau);
+function changeImg(){
+    document.slide.src = images[i];
 
-//Retirer la derniere valeur du tableau
-let valeur = monJoliTableau.pop();
-console.log(monJoliTableau); // it s not necessary to write console.log always it is just to see step by step whats going on if we want
-console.log(valeur);
-
-//Retirer la première valeur d'un tableau
-valeur = monJoliTableau.shift();
-console.log(monJoliTableau);
-console.log(valeur);
-
-//Parcourir tout le valeur d'un tableau n'importe quelle la taille
-for(let ligne = 0; ligne < monJoliTableau.length; ligne++){ //for(let ligne = 0; ligne <= monJoliTableau.length -1; ligne++){
-    console.log(monJoliTableau[ligne]);
-}
-
-//en Javascript on va parcourir le tableau avec boucle fort
+    if(i<images.length - 1){
+        i++;
+    }else{
+        i=0;
+    }
+    setTimeout("changeImg()", time);
+}   
