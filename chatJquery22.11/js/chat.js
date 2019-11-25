09365f92b74ba8ac206860d83fe9990f00b05fbe
curlycarla2004 <curlycarla2004@hotmail.com>
2019-11-25 22:55:34 +0100

    28.11 change

--- chatJquery22.11/js/chat.js
+++ chatJquery22.11/js/chat.js
@@ -1,15 +1,10 @@
 let sushiOk, makiOk,sashimiOk,yakitoriOk,onigriOk
 $(function(){  //there are different versions how to write window.onload
-    $("#chat").on("change",chooseCat);
-    
+
+    $("#chattext").hover(function){
+    $(this).css("background-color","#061339");
+    });
    
 });//fermeture $function
 
-function chooseCat(){
-    if($(this) = "" ){ //
-        $(this).css("border-color", "red"); 
-        
-    }else{
-        $(this).css("border-color", "green"); 
-    }
-}
+
